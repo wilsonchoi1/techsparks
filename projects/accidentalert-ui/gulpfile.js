575c403ed2a086fdb3c9b7ea809b287998e878d5
Hugo Guerrero <hguerrer@hguerrer-osx.local>
2018-01-20 23:09:02 -0600

    added BACKEND_URL to replace on the fly

--- projects/accidentalert-ui/gulpfile.js
+++ projects/accidentalert-ui/gulpfile.js
@@ -93,6 +93,7 @@ gulp.task('html', ['clean'], function () {
         .src([srcPath + '*.html'])
         .pipe(include())
         .pipe(replace('http://localhost:18080', process.env.SSO_URL || 'http://localhost:18080'))
+        .pipe(replace('http://localhost:8080', process.env.BACKEND_URL || 'http://localhost:8080'))
         .on("error", notify.onError({ message: "Error: <%= error.message %>", title: "Error running html task" }))
         .pipe(gulp.dest(distPath));
 });
